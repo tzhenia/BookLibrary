62f325525ee2eafa84fa5c74f0825be16b0ee211
Evhenii Taranukha <tzhenia31@gmail.com>
2018-08-13 13:30:19 +0300

    Base class class with property ID

---
+++ src/main/java/com/tzhenia/bookLibrary/model/BaseEntity.java
@@ -0,0 +1,25 @@
+package com.tzhenia.bookLibrary.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+/**
+ * Base class class with property ID.
+ */
+
+@MappedSuperclass
+@Getter
+@Setter
+@ToString
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
