bbfd281fdc1f062bfec91aa831e50d96b2d802b1
Evhenii Taranukha <tzhenia31@gmail.com>
2018-08-13 13:13:57 +0300

    added sql file for DB migrations

---
+++ src/main/resources/database/initDB.sql
@@ -0,0 +1,28 @@
+CREATE TABLE IF NOT EXISTS books (
+  id         BIGINT AUTO_INCREMENT PRIMARY KEY,
+  name       VARCHAR(50)   NOT NULL,
+  published  DATE          NOT NULL,
+  genre      VARCHAR(50)   NOT NULL,
+  rating     TINYINT(5)    NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS authors (
+  id         BIGINT AUTO_INCREMENT PRIMARY KEY,
+  name       VARCHAR(50)   NOT NULL,
+  gender     VARCHAR(6)    NOT NULL,
+  born       DATE          NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS author_book (
+  id         BIGINT AUTO_INCREMENT PRIMARY KEY,
+  id_books   BIGINT   NOT NULL,
+  id_authors BIGINT   NOT NULL
+);
+
+
+# Data structure:
+# ● Books (id, name, published, genre, rating)
+# ● Authors (id, name, gender, born)
+# ● Book-Author (id, book-id, author-id)
